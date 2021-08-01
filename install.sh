92cdd2127b95f5024f82b0def35fb1319e26785e
Juha <jvierine@gmail.com>
2021-08-01 21:00:54 +0000

    added install autossh as a step

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 
 sudo cp autossh.service /etc/systemd/system/autossh.service
+sudo apt-get install autossh
 sudo systemctl daemon-reload
 sudo systemctl start autossh.service
 sudo systemctl enable autossh.service
